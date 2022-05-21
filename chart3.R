05a9726545ea6086f2541fac6f0bc041e2df7730
phoebermace <phoebermace@gmail.com>
2022-05-20 17:54:54 -0700

    table summary and chart3

---
+++ chart3.R
@@ -0,0 +1,53 @@
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+# bring in datasets
+min_wage_df <- read.csv("minwagedata.csv")
+
+living_wage_df <- read.csv("livingwage.csv")
+
+# create dataset to use in chart
+
+testOne_df <- living_wage_df %>%
+  rename(State = 'state',
+         Living = 'one_adult_no_kids_living_wage') %>%
+  select(State, Living) %>%
+  group_by(State) %>%
+  filter(Living == mean(Living))
+
+testTwo_df <- min_wage_df %>%
+  rename(Minimum = 'State.Minimum.Wage.2020.Dollars') %>%
+  filter(Year == 2020) %>%
+  select(State, Minimum)
+  
+chart_df <- merge(testOne_df, testTwo_df)
+chart_df <- chart_df %>%
+  pivot_longer(
+    -State
+  )
+
+# simplify states to make table easier to read
+chart_df[chart_df$State == "Alaska", "State"] <- "AK"
+chart_df[chart_df$State == "District of Columbia", "State"] <- "DC"
+chart_df[chart_df$State == "Georgia", "State"] <- "GA"
+chart_df[chart_df$State == "Hawaii", "State"] <- "HI"
+chart_df[chart_df$State == "Idaho", "State"] <- "ID"
+chart_df[chart_df$State == "Illinois", "State"] <- "IL"
+chart_df[chart_df$State == "Kansas", "State"] <- "KS"
+chart_df[chart_df$State == "Maryland", "State"] <- "MD"
+chart_df[chart_df$State == "Massachusetts", "State"] <- "MA"
+chart_df[chart_df$State == "Michigan", "State"] <- "MI"
+chart_df[chart_df$State == "Minnesota", "State"] <- "MN"
+chart_df[chart_df$State == "New Jersey", "State"] <- "NJ"
+chart_df[chart_df$State == "New Mexico", "State"] <- "NM"
+chart_df[chart_df$State == "Oregon", "State"] <- "OR"
+
+
+# create chart
+ggplot(chart_df, aes(fill = name, y = value, x = State,)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  labs(title = "Living Wage vs. Minimum Wage", y = 'Dollars') +
+  guides(fill = guide_legend(title = "Type")) +
+  scale_fill_manual(values = c("darkgreen", "darkseagreen4"))
+
---
+++ tablesummary.R
@@ -0,0 +1,73 @@
+# packages used
+library(stringr)
+library(dplyr)
+library(tidyverse)
+
+# bring in datasets
+Zillow_df <- read.csv("Zillow.csv")
+
+statespendingKids_df <- read.csv("statespendingonkids.csv")
+
+min_wage_df <- read.csv("minwagedata.csv")
+
+living_wage_df <- read.csv("livingwage.csv")
+
+# combine and narrow down features
+
+  # state spending on kids dataframe
+statespendingKids_df <- unique(statespendingKids_df %>%
+  pivot_longer(
+    -state) %>%
+  rename(State = 'state',
+         Year = 'name',
+         Public_spending_on_children = 'value'))
+
+statespendingKids_df$Year <- gsub("^.", "", statespendingKids_df$Year)
+
+  # Zillow home value index dataframe
+Zillow_df <- unique(Zillow_df %>%
+  pivot_longer(
+    -X) %>% 
+  rename(State = 'name',
+         Home_value_index = 'value',
+         Year = 'X'))
+
+Zillow_df$Year <- gsub(".{6}$", "", Zillow_df$Year)
+
+
+  # combine minimum wage dataframe with living wage dataframe
+wage_df <- merge(min_wage_df, living_wage_df, by.x = 'State', by.y = 'state')
+
+wage_df <- unique(wage_df %>%
+  rename(State_minimum_wage = 'State.Minimum.Wage.2020.Dollars',
+         Federal_minimum_wage = 'Federal.Minimum.Wage.2020.Dollars',
+         Avg_living_wage = 'one_adult_no_kids_living_wage') %>%
+  select(State, Year, State_minimum_wage, Federal_minimum_wage, Avg_living_wage) %>%
+  arrange(Year)) %>%
+  group_by(Year, State) %>% filter(Avg_living_wage == mean(Avg_living_wage))
+
+
+
+# tables:
+
+# calculates the mean/average home value index for each state by year
+Zillow_mean_df <- Zillow_df %>%
+  group_by(State, Year) %>%
+  summarize(Avg_home_value_index = mean(Home_value_index))
+
+# calculates the total state spending on kids for each state by year
+total_kids_spending <- statespendingKids_df %>%
+  group_by(State, Year) %>%
+    summarize(Public_spending_on_children = sum(Public_spending_on_children)) %>%
+  arrange(Year)
+
+# calculates the state with the highest minimum wage from every year in the dataset 
+highest_each_state <- wage_df %>%
+  group_by(Year) %>%
+  filter(State_minimum_wage == max(State_minimum_wage))
+
+  # then the lowest minimum wage
+lowest_each_state <- wage_df %>%
+  group_by(Year) %>%
+  filter(State_minimum_wage == min(State_minimum_wage))
+
